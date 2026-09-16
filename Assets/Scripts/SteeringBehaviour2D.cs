13b0b86d10d81f7dcb46be810606139fea1a0c7f
Herawisp <138887819+Herawisp@users.noreply.github.com>
2026-09-16 10:17:15 +0700

    Pursue, Evade, Arrive

--- Assets/Scripts/SteeringBehaviour2D.cs
+++ Assets/Scripts/SteeringBehaviour2D.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -13,16 +14,39 @@ public class SteeringBehaviour : MonoBehaviour
     [Header("Behaviour Weights")]
     public float SeekForceWeight;
     public float FleeForceWeight;
+    public float PursueForceWeight;
+    public float EvadeForceWeight;
+    public float ArriveForceWeight;
 
     [Header("Seek Behaviour")]
     public Vector2 SeekTargetPosition;
     
     [Header("Flee Behaviour")]
     public Vector2 FleeTargetPosition;
+    
+    [Header("Pursue Behaviour")]
+    public SteeringBehaviour PursuedAgent;
+    
+    [Header("Evade Behaviour")]
+    public SteeringBehaviour EvadeAgent;
+    
+    [Header("Arrive Behaviour")]
+    public Vector2 ArriveTargetPosition;
+    public float SlowingRadius;
+
+    [Header("Wander Behaviour")]
+    public float WanderDistance;
+    public float WanderPower;
+    public float WanderAngle;
+    public float WanderChange;
 
     void Update() {
         if (SeekForceWeight != 0) ApplyForce(SeekForce(SeekTargetPosition.x, SeekTargetPosition.y), SeekForceWeight);
         if (FleeForceWeight != 0) ApplyForce(FleeForce(FleeTargetPosition.x, FleeTargetPosition.y), FleeForceWeight);
+        if (PursueForceWeight != 0) ApplyForce(PursueForce(PursuedAgent), PursueForceWeight);
+        if (EvadeForceWeight != 0) ApplyForce(EvadeForce(EvadeAgent), EvadeForceWeight);
+        if (ArriveForceWeight != 0) ApplyForce(ArriveForce(ArriveTargetPosition.x, ArriveTargetPosition.y, SlowingRadius), ArriveForceWeight);
+
 
         Velocity += SteeringForce;
         Velocity = Vector2.ClampMagnitude(Velocity, MaxSpeed);
@@ -54,4 +78,41 @@ public class SteeringBehaviour : MonoBehaviour
         vector = Vector2.ClampMagnitude(vector, MaxForce);
         return vector;
     }
+
+    private Vector2 PursueForce(SteeringBehaviour other) {
+        Vector2 vector = other.Velocity;
+        vector *= 10;
+        vector += other.Position;
+        return SeekForce(vector.x, vector.y);
+    }
+
+    private Vector2 EvadeForce(SteeringBehaviour other) {
+        Vector2 vector = other.Velocity;
+        vector *= 10;
+        vector += other.Position;
+        return FleeForce(vector.x, vector.y);
+    }
+
+    private Vector2 ArriveForce(float x, float y, float slowingRadius) {
+        Vector2 vector = new(x, y);
+        vector -= Position;
+
+        float dist = vector.magnitude;
+        if (dist > slowingRadius) {
+            vector = vector.normalized * MaxSpeed;
+        } else {
+            vector = vector.normalized * (dist / slowingRadius);
+        }
+
+        vector -= Velocity;
+        vector = Vector2.ClampMagnitude(vector, MaxForce);
+        return vector;
+    }
+
+    // private Vector2 WanderForce() {
+    //     Vector2 vector = Velocity;
+    //     vector = vector.normalized * WanderDistance;
+        
+    //     float radians = ()
+    // }
 }
